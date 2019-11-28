86429915213e79b4216c5f2ad46612b200ddd768
Gabriel Loureiro <gabrieloureirof@gmail.com>
2019-11-28 09:55:14 -0300

    commit bg video

---
+++ lib/forgotpassword.dart
@@ -0,0 +1,127 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/model/user.dart';
+
+import 'model/size_config.dart';
+class ForgotPassword extends StatefulWidget {
+  ForgotPassword({Key key}) : super(key: key);
+
+  @override
+  _ForgotPasswordState createState() => _ForgotPasswordState();
+}
+//to do respota para o usuario de reset da senha
+class _ForgotPasswordState extends State<ForgotPassword> {
+  TextEditingController _controllerEmail = TextEditingController();
+  String _erroMessage = "";
+
+  _forgotPassword(User user){
+      FirebaseAuth auth = FirebaseAuth.instance;
+      auth.sendPasswordResetEmail(
+        email: user.email,
+      ).then((firebaseUser){
+        Navigator.pushNamed(context, "/login");
+
+      }).catchError((e){
+        _erroMessage = "E-mail invalido";
+      });
+  }
+  _validateFields(){
+    String email = _controllerEmail.text;
+
+    //VALIDACAO DOS CAMPOS
+     if(email.isNotEmpty && email.contains("@") && !email.contains(" ")){
+
+          User user = User();
+          user.email = email;
+          _forgotPassword(user);
+          _erroMessage = '';
+      
+      }else{
+        setState(() {
+          _erroMessage = "Endereço de e-mail inválido";
+        });
+      }
+    
+
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.black87, Colors.grey],
+            )
+          ),
+          alignment: Alignment.topCenter,
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Recuperar Senha"),
+            backgroundColor: Colors.black87,
+          ),
+          body: Container( 
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.black87, Colors.grey],
+              )
+            ),       
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                TextField(
+                controller: _controllerEmail,
+                autofocus: true,
+                keyboardType: TextInputType.emailAddress,
+                style: TextStyle(fontSize: SizeConfig.blockSizeHorizontal*3.3),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.fromLTRB(15, 16, 32, 16),
+                  hintText: "E-mail",
+                  filled: true,
+                  fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32),
+                                
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.alternate_email,
+                      color: Colors.black54,
+                    )
+                  ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16, bottom: 10),
+                child: RaisedButton(
+                  child: Text(
+                      "Recuperar",
+                    style: TextStyle(color: Colors.white, 
+                    fontSize: SizeConfig.blockSizeHorizontal*4),
+                  ),
+                    color: Color(0xff1ebbd8),
+                    padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    onPressed: (){
+                      _validateFields();
+                    }
+                ),
+              ),
+            ], 
+          ),
+        ),
+        )
+      ],
+    );
+  }
+}
+
--- lib/login.dart
+++ lib/login.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/model/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_app/model/size_config.dart';
+import 'package:video_player/video_player.dart';
+
 
 class Login extends StatefulWidget {
   @override
@@ -11,7 +13,16 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   TextEditingController _controllerEmail = TextEditingController();
   TextEditingController _controllerSenha = TextEditingController();
+  VideoPlayerController _controllerVideo;
   String _erroMessage = '';
+  bool _obscureText = true;
+
+
+   void _passwordText(){
+    setState(() {
+      _obscureText = ! _obscureText;
+    });
+  }
   
   _validateFields(){
     String email = _controllerEmail.text;
@@ -20,7 +31,7 @@ class _LoginState extends State<Login> {
     //VALIDACAO DOS CAMPOS
      if(email.isNotEmpty && email.contains("@") && !email.contains(" ")){
 
-        if(password.isNotEmpty){
+        if(password.isNotEmpty && !password.contains(" ")){
 
           User user = User();
           user.email = email;
@@ -39,7 +50,6 @@ class _LoginState extends State<Login> {
           _erroMessage = "Endereço de e-mail inválido ou já utilizado";
         });
       }
-    //LOGANDO USUARIO NO FIREBASE
    
 
   }
@@ -55,106 +65,163 @@ class _LoginState extends State<Login> {
       });
     }
 
+    @override
+    void initState() {
+    super.initState();
+    _controllerVideo = VideoPlayerController.network(
+        'https://firebasestorage.googleapis.com/v0/b/seven7cares.appspot.com/o/background%2FACADEMIA-SEVEN-CARE_2.mp4?alt=media&token=da30e3ee-1dbb-4723-b798-ff047576894e')
+      ..initialize().then((_) {
+        _controllerVideo.play();
+        _controllerVideo.setLooping(true);
+        // Ensure the first frame is shown after the video is initialized
+        setState(() {});
+      });
+  }
+
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controllerVideo.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Colors.black87, Colors.grey],
-            )
-          ),
-        alignment: Alignment.topCenter,
-        padding: EdgeInsets.all(16),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(bottom: 32),
-                  child: Image.asset(
-                      "images/logo-s7.png",),
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          alignment: Alignment.topCenter,
+          children: <Widget>[
+            SizedBox.expand(
+              child: FittedBox(
+                fit: BoxFit.cover,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  child: VideoPlayer(_controllerVideo),
                 ),
-                TextField(
-                  controller: _controllerEmail,
-                  autofocus: true,
-                  keyboardType: TextInputType.emailAddress,
-                  style: TextStyle(fontSize: SizeConfig.blockSizeHorizontal*3.3),
-                  decoration: InputDecoration(
-                      contentPadding: EdgeInsets.fromLTRB(25, 16, 32, 16),
-                      hintText: "E-mail",
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(32),
-                      )
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextField(
-                  controller: _controllerSenha,
-                  obscureText: true,
-                  keyboardType: TextInputType.emailAddress,
-                  style: TextStyle(fontSize: SizeConfig.blockSizeHorizontal*3.3),
-                  decoration: InputDecoration(
-                      contentPadding: EdgeInsets.fromLTRB(25, 16, 32, 16),
-                      hintText: "Senha",
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(32)
-                      )
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 16, bottom: 10),
-                  child: RaisedButton(
-                    child: Text(
-                        "Entrar",
-                      style: TextStyle(color: Colors.white, 
-                      fontSize: SizeConfig.blockSizeHorizontal*4),
-                    ),
-                      color: Color(0xff1ebbd8),
-                      padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(32),
-                      ),
-                      onPressed: (){
-                        _validateFields();
-                      }
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                        "Não possui conta?\t",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    GestureDetector(
-                      child: Text("Cadastre-se!",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),  
-                      ),
-                    onTap: (){
-                      Navigator.pushNamed(context, "/signup");
-                    },
-                  ),
-                  ],
-                )
-              ],
+              ),
             ),
-          ),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Padding(
+                      padding: EdgeInsets.only(bottom: 32),
+                      child: Image.asset(
+                          "images/logo-s7.png",),
+                      ),
+                      TextField(
+                        controller: _controllerEmail,
+                        autofocus: false,
+                        keyboardType: TextInputType.emailAddress,
+                        style: TextStyle(fontSize: SizeConfig.blockSizeHorizontal*3.3),
+                        decoration: InputDecoration(
+                            contentPadding: EdgeInsets.fromLTRB(25, 16, 32, 16),
+                            hintText: "E-mail",
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(32),
+                                
+                            ),
+                            prefixIcon: const Icon(
+                              Icons.alternate_email,
+                              color: Colors.black54,
+                            )
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextField(
+                        controller: _controllerSenha,
+                        obscureText: _obscureText,
+                        keyboardType: TextInputType.emailAddress,
+                        style: TextStyle(fontSize: SizeConfig.blockSizeHorizontal*3.3),
+                        decoration: InputDecoration(
+                            contentPadding: EdgeInsets.fromLTRB(25, 16, 32, 16),
+                            hintText: "Senha",
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(32)
+                            ),
+                            prefixIcon: IconButton(
+                                  icon : Icon(_obscureText ? Icons.remove_red_eye : Icons.panorama_fish_eye),
+                                  color: Colors.black45,
+                                  onPressed: _passwordText,
+                            ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 16, bottom: 10),
+                        child: RaisedButton(
+                          child: Text(
+                              "Entrar",
+                            style: TextStyle(color: Colors.white, 
+                            fontSize: SizeConfig.blockSizeHorizontal*4),
+                          ),
+                            color: Color(0xff1ebbd8),
+                            padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(32),
+                            ),
+                            onPressed: (){
+                              _validateFields();
+                            }
+                        ),
+                      ),
+                    
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                              "Não possui conta?\t",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          GestureDetector(
+                            child: Text("Cadastre-se!",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),  
+                            ),
+                          onTap: (){
+                            Navigator.pushNamed(context, "/signup");
+                          },
+                        ),
+                        
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                          child: GestureDetector(
+                            child: Text("Esqueci a senha",
+                            style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          onTap: (){
+                            Navigator.pushNamed(context, "/forgot");
+                          },
+                          )
+                        )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-      ),
-    );
+      );
+    }
   }
-}
