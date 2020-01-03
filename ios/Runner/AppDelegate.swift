151aab3bbf7a2d5c26cafdf227e80b5aa9d2b0a8
Evox Group <gabrieloureiro@Evox-Macbook-Air.local>
2020-01-03 13:05:59 -0300

    Issue fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,6 @@
 import UIKit
 import Flutter
-import FBSDKLoginKit
-import FBSDKCoreKit
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,19 +8,8 @@ import FBSDKCoreKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
-
+    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions) 
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-      @available(iOS 9.0, *)
-  override  func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any])
-        -> Bool {
-            return FBSDKApplicationDelegate.sharedInstance().application(application,  open: url, sourceApplication:options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String!,annotation: options[UIApplicationOpenURLOptionsKey.annotation])
-    }
-
-    // para iOS menor a 9.0
-  override func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-        return FBSDKApplicationDelegate.sharedInstance().application(application,open: url as URL!,sourceApplication: sourceApplication,annotation: annotation)
-    }
 }
